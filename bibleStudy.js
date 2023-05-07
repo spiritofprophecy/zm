c5282840fcd8ef9fa7a8bb2a4696103368814e8d
spiritofprophecy <132688646+spiritofprophecy@users.noreply.github.com>
2023-05-07 18:33:00 +0200

    Add files via upload

---
+++ bibleStudy.js
@@ -0,0 +1,5 @@
+let hamburgMenu = document.querySelector('.hamburg-menu');
+let tracks = document.querySelector('.tracks');
+hamburgMenu.addEventListener('click', function() {
+  tracks.style.display = 'block';
+});
